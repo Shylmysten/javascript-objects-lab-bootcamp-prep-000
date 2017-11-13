4a5519e4c0dc1efe8227d6fd6927322d924f9a26
Tony Boswell <tboswell@kc.rr.com>
2017-11-13 16:21:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function updateObjectWithKeyAndValue(obj, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
-  obj.[key] = value;
+  obj.key = value;
   return obj;
 }
