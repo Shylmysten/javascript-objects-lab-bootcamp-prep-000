1a4398ed120aeb4020598d0151d820fefe75192a
Tony Boswell <tboswell@kc.rr.com>
2017-11-13 16:23:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,15 +3,3 @@ var recipes = {};
 // creates a function that updates an Object with a new key value
 
 
-
-
-// creates a function that clones another object, updates and returns only the clone's data
-
-function updateObjectWithKeyAndValue(obj, key, value) {
-  return Object.assign({},obj, {[key]:value})
-}
-
-function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
-  obj[key] = value;
-  return obj;
-}
