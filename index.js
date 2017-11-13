67b74ef299d976e0906ed3e90753ee1e33cc2d77
Tony Boswell <tboswell@kc.rr.com>
2017-11-13 16:09:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var recipes = {};
+const var recipes = {};
 
 // creates a function that updates an Object with a new key value
 
@@ -11,6 +11,5 @@ function updateObjectWithKeyAndValue(object, key, value) {
 // creates a function that clones another object and returns the clone's data
 
 function updateObjectWithKeyAndValue(object, key, value) {
-
 
 }
