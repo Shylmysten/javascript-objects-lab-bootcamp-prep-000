c1032e135d5960fef4ade861a54d05b512b466ab
Tony Boswell <tboswell@kc.rr.com>
2017-11-13 15:35:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 var recipes = {};
+
+// creates a function that updates an Object with a new key value
+
+function updateObjectWithKeyAndValue(obj, key, value) {
+  obj[key]= value;
+  return obj;
+}
