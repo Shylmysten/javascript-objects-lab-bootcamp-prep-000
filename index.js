ce94fb8f702bd9fc96ddd51e6aacb1819cc223e7
Tony Boswell <tboswell@kc.rr.com>
2017-11-13 16:09:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,4 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
 // creates a function that clones another object and returns the clone's data
 
-function updateObjectWithKeyAndValue(object, key, value) {
 
-}
